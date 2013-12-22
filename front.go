847f667b460586d9dd9d3672db4d31c8a0528dae
Sasha Crofter <sasha@crofter.org>
2013-12-22 12:43:29 -0500

    Add convenience function for RelativeWeekday

--- front.go
+++ front.go
@@ -11,3 +11,7 @@ func FormatRelative(format string, t time.Time) string {
 func RelativeDay(t time.Time) string {
 	return RelTime(time.Now()).RelativeDay(t)
 }
+
+func RelativeWeekday(t time.Time) string {
+	return RelTime(time.Now()).RelativeWeekday(t)
+}
