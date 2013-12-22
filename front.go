ae7bd0787727e9e2a979666d17bdbb7723d5eabb
Sasha Crofter <sasha@crofter.org>
2013-12-22 02:32:19 -0500

    Add front.go for global convenience functions

---
+++ front.go
@@ -0,0 +1,13 @@
+package reltime
+
+import (
+	"time"
+)
+
+func FormatRelative(format string, t time.Time) string {
+	return RelTime(time.Now()).FormatRelative(format, t)
+}
+
+func RelativeDay(t time.Time) string {
+	return RelTime(time.Now()).RelativeDay(t)
+}
